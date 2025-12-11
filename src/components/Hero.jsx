366887cf697a7b0092bb4bdbd2182477d7bdc02a
TharuHima <hathimadhya@gmail.com>
2025-12-11 11:19:46 +0530

    uploading profile picture

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import ProfilePic from '../assets/profile.jpg';
 
 function Hero() {
   return (
     <section id="hero" className="hero-section">
       <div className="hero-content">
+        <img src={ProfilePic} className="profile-pic" alt="Profile Picture" />
         <h1>Hi, I'm Tharushi Himadhya</h1>
         <h2>Software Developer | Designer</h2>
         <p className="bio-summary">
