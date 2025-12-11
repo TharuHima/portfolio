80877b3f9c6d15442811ec75ff0717464659c1ef
TharuHima <hathimadhya@gmail.com>
2025-12-11 12:00:23 +0530

    changed some texts

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ function Contact() {
     <section id="contact" className="contact-section">
       <h2>Contact Me</h2>
       <p>
-        I'm always open to discussing new projects, creative ideas, or opportunities to be part of your visions. Feel free to reach out!
+        I'm always open to discuss new projects, creative ideas, or opportunities to be part of your visions. Feel free to reach out!
       </p>
       <ul className="contact-info">
         <a className="contact-link" href="mailto:hathimadhya@gmail.com" target="_blank">Email</a>
